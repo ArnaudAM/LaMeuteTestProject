91e30863f3e954a2b3b5a10ba6fbe01115580bce
Didier Dardevet <didier.dardevet@gmail.com>
2022-05-10 21:10:33 +0200

    change common using login command

--- cypress/integration/common/a_logged_buyer.js
+++ cypress/integration/common/a_logged_buyer.js
@@ -1,10 +1,20 @@
 /// <reference types="cypress" />
 
 import { Given } from "cypress-cucumber-preprocessor/steps";
+let users = require("../../fixtures/users")
+let email = users[2]["buyer"]["email"]
+let password = users[2]["buyer"]["password"] 
+
+// Given("a logged buyer", () => {
+//   cy.visit("/");
+//   cy.expect(cy.get('[id="dropdown-account"]').should("be.visible").click());
+//   cy.get('[id="UserLoginDropdown"]').type("byergp3@data.fr");
+//   cy.get('[id="UserPasswordDropdown"]').type("test {enter}");
+// });
 
 Given("a logged buyer", () => {
-  cy.visit("/");
-  cy.expect(cy.get('[id="dropdown-account"]').should("be.visible").click());
-  cy.get('[id="UserLoginDropdown"]').type("byergp3@data.fr");
-  cy.get('[id="UserPasswordDropdown"]').type("test {enter}");
+  cy.login(email, password).then(response => {
+    expect(response.status).to.eq(200)
+  })
+  cy.visit("/")
 });
