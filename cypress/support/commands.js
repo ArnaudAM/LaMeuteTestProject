d8f539c5054e7283add65af966777d104fa919e4
ArnaudAM <arnaud.auzoudm@gmail.com>
2022-05-11 12:57:10 +0200

    Modif commands.js to add a workflow created function, add a workflows.json file and modify adminNewPNICFWorflow.spec.js to use the function

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,3 +17,14 @@ Cypress.Commands.add("deconnexion", () => {
   cy.wait(2000);
   cy.get('[data-test-id="signOutButton"]').click({ force: true });
 });
+
+Cypress.Commands.add("createWorkflow", (code, workflowName, trigger, max, validator, min) => {
+  cy.get('#AdminWorkflows > .flex > a > .text-lightGrey').click()
+  cy.get('[id="workflow_id"]').type(code)
+  cy.get('[id="workflow_name"]').type(workflowName)
+  cy.get('[type="checkbox"]').check({force:true})
+  cy.get('[id="workflow_trigger"]').select(trigger)
+  cy.get('[id="workflow_max"]').type(max)
+  cy.get('[id="workflow_validator1"]').select(validator)
+  cy.get('[id="workflow_min"]').type(min)
+})
