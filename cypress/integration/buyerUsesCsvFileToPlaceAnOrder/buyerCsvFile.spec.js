03390c229b03d54597dc5897fc5c64b814ffd855
geralddcx <gerald.ducloux@gmail.com>
2022-05-11 10:50:04 +0200

    ajout csv

---
+++ cypress/integration/buyerUsesCsvFileToPlaceAnOrder/buyerCsvFile.spec.js
@@ -0,0 +1,6 @@
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+
+And(" the buyer checks his cart", () => {});
+// When he add a csv file order
+// Then the cart should be updated from csv articles
+// And their number should be equal to the csv file
